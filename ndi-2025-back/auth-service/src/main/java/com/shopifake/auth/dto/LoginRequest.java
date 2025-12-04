6a09f27ba60c937b8e5e94ea17a4279c5e51dd9a
Alexandre Fleury <alexx97354@gmail.com>
2025-12-04 19:40:35 +0100

    first commit

---
+++ ndi-2025-back/auth-service/src/main/java/com/shopifake/auth/dto/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.shopifake.auth.dto;
+
+import lombok.Data;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+    private String username;
+    private String password;
+}
