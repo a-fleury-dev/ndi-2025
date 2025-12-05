3317dcae6d471e93270de70f49b7e3a7a52a67af
Camille Renard <camille.renard@etu.umontpellier.fr>
2025-12-05 07:01:47 +0100

    feat: change app title

--- ndi-2025-front/app/root.tsx
+++ ndi-2025-front/app/root.tsx
@@ -11,6 +11,7 @@ import type { Route } from "./+types/root";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
+  { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
@@ -21,6 +22,10 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+];
+
+export const meta: Route.MetaFunction = () => [
+  { title: "ndi-zigottos" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
